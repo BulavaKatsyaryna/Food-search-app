12718158a7b0336eb3d4ffff1425866f634d3380
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-05-06 00:53:30 +0300

    Add files via upload

---
+++ food-search-app/src/test/java/com/example/foodsearchapp/FoodSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodsearchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSearchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
