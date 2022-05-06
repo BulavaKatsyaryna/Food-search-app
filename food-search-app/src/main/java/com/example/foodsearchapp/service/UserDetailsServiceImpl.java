f18848a1b8e9fdbbec1425b6c8ffe7b142d25841
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-05-07 00:38:49 +0300

    Written service for user authorization

---
+++ food-search-app/src/main/java/com/example/foodsearchapp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.foodsearchapp.service;
+
+import com.example.foodsearchapp.model.User;
+import com.example.foodsearchapp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    User user;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with this username not found: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
