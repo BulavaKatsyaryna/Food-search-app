e9eeff24e58c72627e9a812315c129d65e1a2f04
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-05-07 02:39:24 +0300

    The problem in Jwts.builder().signWith is deprecated. Not resolved

--- food-search-app/src/main/java/com/example/foodsearchapp/config/token/AuthTokenFilter.java
+++ food-search-app/src/main/java/com/example/foodsearchapp/config/token/AuthTokenFilter.java
@@ -1,6 +1,15 @@
 package com.example.foodsearchapp.config.token;
 
 
+import com.example.foodsearchapp.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,9 +20,41 @@ import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Autowired
+    private Utility utility;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        try {
+            String jsonWebToken = parseJsonWebToken(request);
+            if (jsonWebToken != null && utility.validateJsonWebToken(jsonWebToken)) {
+                String username = utility.getUserNameFromJsonWebToken(jsonWebToken);
 
+                UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }catch (Exception e) {
+            log.error(String.valueOf(e));
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJsonWebToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
     }
 }
---
+++ food-search-app/src/main/java/com/example/foodsearchapp/config/token/Utility.java
@@ -0,0 +1,50 @@
+package com.example.foodsearchapp.config.token;
+
+import com.example.foodsearchapp.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class Utility {
+
+    private static final Logger log = LoggerFactory.getLogger(Utility.class);
+
+    @Value("${app.jsonWebTokenSecret}")
+    private String jsonWebTokenSecret;
+
+    @Value("${app.jsonWebTokenExpirationMillisecond}")
+    private int jsonWebTokenExpirationMillisecond;
+
+    public String generateJsonWebToken(Authentication authentication) {
+
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jsonWebTokenExpirationMillisecond))
+                .signWith(SignatureAlgorithm.HS512, jsonWebTokenSecret)
+                .compact();
+    }
+
+    public boolean validateJsonWebToken(String jwt) {
+        try {
+            Jwts.parser().setSigningKey(jsonWebTokenSecret).parseClaimsJws(jwt);
+            return true;
+        } catch (MalformedJwtException | IllegalArgumentException e) {
+            log.error(e.getMessage());
+        }
+
+        return false;
+    }
+
+    public String getUserNameFromJsonWebToken(String jwt) {
+        return Jwts.parser().setSigningKey(jsonWebTokenSecret).parseClaimsJws(jwt).getBody().getSubject();
+    }
+}
